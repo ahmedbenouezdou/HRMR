efddc47e23b4f68116d8e0374a62bd3b797539d4
Ahmed ben ouezdou <ahmed.b.ouezdou@gmail.com>
2017-07-30 01:22:49 +0200

    calendar update

---
+++ src/module/myProfil/myProfilComponent.js
@@ -0,0 +1,11 @@
+function myProfilController() {
+    var ctrl = this;
+
+}
+
+angular.module('myProfilModule').component('myProfilComponent', {
+    templateUrl: 'src/module/myProfil/myProfil.html',
+    controller: myProfilController,
+    controllerAs: 'ctrl'
+
+});
